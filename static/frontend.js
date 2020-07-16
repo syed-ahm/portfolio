bf4737085805a0ecdc4c1cb85782ef4d74cf65f5
Syed Ahmed <syed-hamdan@hotmail.com>
2020-07-15 22:43:52 -0400

    added comments

--- static/frontend.js
+++ static/frontend.js
@@ -1,13 +1,21 @@
+// Grabbing arrow image elements
 var projectsButton = document.getElementById('projectsButton');
 var resumeButton = document.getElementById('resumeButton');
 
 
+// Removes arrow image animations/delay so new animations can be added 
+setTimeout(function() {
+    projectsButton.classList.remove('animate__bounceInDown', 'animate__delay-5s');
+    resumeButton.classList.remove('animate__bounceInUp', 'animate__delay-5s');
+}, 6000)
+
+
+// Adding onmouseover events on arrow images for shake animation
 projectsButton.addEventListener('mouseover', projectsShake);
 resumeButton.addEventListener('mouseover', resumeShake);
 
 
-
-
+// Adds the shake animation and removes after 1s
 function projectsShake() {
     projectsButton.classList.add('animate__swing');
     var wait = setTimeout(function() {
@@ -16,8 +24,7 @@ function projectsShake() {
 }
 
 
-
-
+// Adds the shake animation and removes after 1s
 function resumeShake() {
     resumeButton.classList.add('animate__swing');
     var wait = setTimeout(function() {
@@ -26,19 +33,22 @@ function resumeShake() {
 }
 
 
-setTimeout(function() {
-    projectsButton.classList.remove('animate__bounceInDown', 'animate__delay-5s');
-}, 6000)
-
-setTimeout(function() {
-    resumeButton.classList.remove('animate__bounceInUp', 'animate__delay-5s');
-}, 6000)
 
 
 
-var intro1 = document.getElementById('intro1');
-var intro2 = document.getElementById('intro2');
-var intro3 = document.getElementById('intro3');
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
