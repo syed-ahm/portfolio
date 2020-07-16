2afcf83f80101e758b299d15854373cefd045198
Syed Ahmed <syed-hamdan@hotmail.com>
2020-07-16 03:49:24 -0400

    cleaned up homepage, started project page

--- mainflask.py
+++ mainflask.py
@@ -2,17 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# /home opens home.html
 @app.route("/home")
 @app.route("/")
 def home():
     return render_template('home.html')
 
+# attempt to stop browser caching
 @app.after_request
 def add_header(response):
     response.cache_control.no_store = True
     return response
-    
 
+# starts flask app
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
     
--- static/frontend.js
+++ static/frontend.js
@@ -1,36 +1,35 @@
-// Grabbing arrow image elements
-var projectsButton = document.getElementById('projectsButton');
-var resumeButton = document.getElementById('resumeButton');
+// Grabbing arrow image element
+var nextButton1 = document.getElementById('nextButton1');
+var nextButton2 = document.getElementById('nextButton2');
 
-
-// Removes arrow image animations/delay so new animations can be added 
+// Removes arrow image animation/delay so new animations can be added 
 setTimeout(function() {
-    projectsButton.classList.remove('animate__bounceInDown', 'animate__delay-5s');
-    resumeButton.classList.remove('animate__bounceInUp', 'animate__delay-5s');
+    nextButton1.classList.remove('animate__bounceInDown', 'animate__delay-5s');
 }, 6000)
 
 
 // Adding onmouseover events on arrow images for shake animation
-projectsButton.addEventListener('mouseover', projectsShake);
-resumeButton.addEventListener('mouseover', resumeShake);
+nextButton1.addEventListener('mouseover', projectsShake);
 
 
 // Adds the shake animation and removes after 1s
 function projectsShake() {
-    projectsButton.classList.add('animate__swing');
+    nextButton1.classList.add('animate__swing');
     var wait = setTimeout(function() {
-        projectsButton.classList.remove('animate__swing');
+        nextButton1.classList.remove('animate__swing');
     }, 1000);
 }
 
 
-// Adds the shake animation and removes after 1s
-function resumeShake() {
-    resumeButton.classList.add('animate__swing');
-    var wait = setTimeout(function() {
-        resumeButton.classList.remove('animate__swing');
-    }, 1000);
-}
+
+
+
+
+
+
+
+
+
 
 
 
